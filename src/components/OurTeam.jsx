b459ccb657e3b76d56e0cc17fa8056d42bb7df74
mustafa-bilen <mustafabilen6669@gmail.com>
2022-09-24 21:13:31 +0300

    debug

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Icons from "../components/Icons";
-import { FiTwitter, FiFacebook, FiLinkedin } from "react-icons/fi";
 
 const OurTeam = () => {
   return (
@@ -16,27 +14,7 @@ const OurTeam = () => {
       {/* sa */}
       <div className="düzenlencek">
         {/*  */}
-        <div className="flex flex-col justify-center items-center">
-          <img
-            src="https://ca.slack-edge.com/T03LBL87DA8-U03MW5VGEMS-e7da38ceffb6-512"
-            my
-            neck
-            hurts
-            so
-            much
-            alt="profil"
-            className="w-[15rem] rounded-full border-[8px] border-gray-400"
-          />
-          <div className="flex flex-col items-center">
-            <p className="font-semibold text-2xl">Frank - Mentor</p>
-            <p className="hover:underline">Back-end Developer</p>
-            <div className="mt-2 flex gap-3">
-              <Icons icon={<FiTwitter />} />
-              <Icons icon={<FiFacebook />} />
-              <Icons icon={<FiLinkedin />} />
-            </div>
-          </div>
-        </div>
+
         {/*  */}
       </div>
     </div>
--- src/components/OurTeamCart.jsx
+++ src/components/OurTeamCart.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import Icons from "../components/Icons";
+import { FiTwitter, FiFacebook, FiLinkedin } from "react-icons/fi";
 const OurTeamCart = () => {
-  return <div>OurTeamCart</div>;
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center">
+        <img
+          src="https://ca.slack-edge.com/T03LBL87DA8-U03MW5VGEMS-e7da38ceffb6-512"
+          alt="profil"
+          className="w-[15rem] rounded-full border-[8px] border-gray-400"
+        />
+        <div className="flex flex-col items-center">
+          <p className="font-semibold text-2xl">Frank - Mentor</p>
+          <p className="hover:underline">Back-end Developer</p>
+          <div className="mt-2 flex gap-3">
+            <Icons icon={<FiTwitter />} />
+            <Icons icon={<FiFacebook />} />
+            <Icons icon={<FiLinkedin />} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurTeamCart;
